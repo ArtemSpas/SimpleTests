92ebffd03d4e26033b2f3e59382bf9d48f313759
ArtemSpas <85579495+ArtemSpas@users.noreply.github.com>
2021-06-08 23:15:20 +0200

    Add files via upload

---
+++ jsonformat.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Book struct {
+	Title  string `json:"title"`
+	Author Author `json:"author"`
+}
+
+type Author struct {
+	Name      string `json:"name"`
+	Age       int    `json:"age"`
+	Developer bool   `json:"is_developer"`
+}
+
+func main() {
+
+	author := Author{Name: "Elliot Forbes", Age: 25, Developer: true}
+	book := Book{Title: "Learning Concurrency in Golang", Author: author}
+
+	byteArray, err := json.MarshalIndent(book, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(byteArray))
+
+}
